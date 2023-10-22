c615fb92a6d9d50b682b59e77f80115d0983a9c4
Gaston Gianni <ggianni@cpqi.com.ar>
2023-10-22 20:30:37 -0300

    revertir

--- src/components/common/PokemonCard.tsx
+++ src/components/common/PokemonCard.tsx
@@ -1,8 +1,8 @@
-"use client";
+'use client';
 
-import React, { FC, useEffect, useState } from "react";
-import Image from "next/image";
-import { PokemonCardType } from "@/types/types";
+import React, { FC, useEffect, useState } from 'react';
+import Image from 'next/image';
+import { PokemonCardType } from '@/types/types';
 // import { fetchTypes } from "@/libs/fetchTypes";
 
 type Props = {
@@ -22,7 +22,7 @@ const PokemonCard: FC<Props> = ({ card }) => {
   //   };
 
   return (
-    <div className="flex flex-col items-center p-4">
+    <div className="flex flex-col items-center p-4 hover:scale-[300%] transition-all">
       <p className="dark:text-white">{card.name}</p>
       <Image className="relative" alt={card.name} src={card.images.large} width={250} height={250} />
     </div>
